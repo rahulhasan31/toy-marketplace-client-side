8610a18a19f4fb003611a60eee2ee5eb16fda149
rahulhasan31 <rahulhasan666@gmail.com>
2023-05-21 09:02:28 +0600

    added

--- src/Component/MyToys/MyToys.js
+++ src/Component/MyToys/MyToys.js
@@ -55,8 +55,10 @@ const MyToys = () => {
   }
   return (
     <>
-         <div className="px-4  mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  mt-5">
-      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+         <div data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500" className="px-4  mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  mt-5">
+      <div  className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
             Toy Brand new
@@ -76,6 +78,9 @@ const MyToys = () => {
       <div className="mx-auto lg:max-w-2xl">
         <div className="relative w-full transition-shadow duration-300 hover:shadow-xl">
           <img
+          data-aos="flip-left"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="2000"
             className="object-cover w-full h-56 rounded p-5 sm:h-64 md:h-80 lg:h-96"
             src="https://i.pinimg.com/564x/82/36/86/82368662c2400c206e4f874e39347b91.jpg"
             alt=""
